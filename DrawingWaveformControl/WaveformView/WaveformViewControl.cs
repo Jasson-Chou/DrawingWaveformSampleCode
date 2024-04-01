026dda6f190880517d566a14bdbfec11288477b6
Jasson_Chou <ss5014871@gmail.com>
2024-04-01 23:46:41 +0800

    first upload

---
+++ DrawingWaveformControl/WaveformView/WaveformViewControl.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WaveformView
+{
+    public class WaveformViewControl : Control
+    {
+        internal WaveformViewControl() 
+        { 
+            
+        }
+
+        private double hornizontalValue;
+        public double HornizontalValue 
+        {
+            get => hornizontalValue;
+            set
+            {
+                hornizontalValue = value;
+                this.InvalidateVisual();
+            }
+        }
+
+        private double verticalValue;
+        public double VerticalValue 
+        {
+            get => verticalValue;
+            set
+            {
+                verticalValue = value;
+                this.InvalidateVisual();
+            }
+        }
+
+        protected override void OnDpiChanged(DpiScale oldDpi, DpiScale newDpi)
+        {
+            base.OnDpiChanged(oldDpi, newDpi);
+            this.InvalidateVisual();
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            _mouseEnter = true;
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            _mouseEnter = false;
+        }
+
+
+        protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseRightButtonDown(e);
+            _mouseRightBtnDown = false;
+        }
+
+        protected override void OnMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseRightButtonUp(e);
+            _mouseRightBtnDown = true;
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+            _mouseLeftBtnDown = true;
+        }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonUp(e);
+            _mouseLeftBtnDown = false;
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+            _mouseLocation = e.GetPosition(this);
+        }
+
+        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseRightButtonDown(e);
+            _mouseRightBtnDown = true;
+        }
+
+        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseRightButtonUp(e);
+            _mouseRightBtnDown = false;
+        }
+
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnPreviewMouseWheel(e);
+            this.InvalidateVisual();
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            this.InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+
+        public void Update()
+        {
+            this.InvalidateVisual();
+        }
+
+        private bool _mouseEnter = false;
+
+        private bool _mouseLeftBtnDown = false;
+
+        private bool _mouseRightBtnDown = false;
+
+        private Point _mouseLocation;
+    }
+}
